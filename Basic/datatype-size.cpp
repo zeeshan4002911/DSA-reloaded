434aeb3fb729a16b53ada663df0c74b6f7686f4a
AAA4002911 <zeeshan4002911@gmail.com>
2024-05-08 07:14:08 +0530

    added

--- Basic/datatype-size.cpp
+++ Basic/datatype-size.cpp
@@ -1,3 +1,28 @@
+/*
+Data type refers to the type of value a variable has and the way the computer interprets it.
+
+Each data type has a different size. You’ve studied 5 different data types and the sizes of the data types:
+
+Integer: 4 bytes
+Long: 8 bytes
+Float: 4 bytes
+Double: 8 bytes
+Character: 1 byte
+
+
+You’re given a data type. Print its size in bytes.
+
+
+Example :
+
+Input: Long
+
+Output: 8
+
+Explanation: The size of a Long variable is given as 8 bytes.
+
+*/
+
 #include<iostream>
 #include<string>
 
