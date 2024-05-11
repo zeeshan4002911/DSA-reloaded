62bda1f24aabb4de07cb3f4abd43c8a6d33c4bf5
AAA4002911 <zeeshan4002911@gmail.com>
2024-05-11 23:24:50 +0530

    added

---
+++ Basic/Pattern/p4.cpp
@@ -0,0 +1,27 @@
+/**
+Example:
+
+Input: ‘N’ = 3
+
+Output: 
+1
+2 2 
+3 3 3
+ 
+*/
+
+#include<iostream>
+
+int main() {
+    int n;
+    std::cout << "Enter N for pattern : ";
+    std::cin >> n; 
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j < i + 1; j++) {
+            std::cout << i << " ";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
+}
