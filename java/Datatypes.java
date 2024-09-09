a5c8262436637bdabfb402bc2b2e383e5cda1538
root <root@BDC7-L-33169B9.dir.svc.accenture.com>
2024-09-09 07:17:44 +0530

    datatypes added

---
+++ java/Datatypes.java
@@ -0,0 +1,40 @@
+/*
+
+Try the following example in the editor below.
+
+You have to assign the following values in the variable:
+
+"Name" should be assigned in the variable named mystring1.
+"Don't change the variable name" should be assigned in the variable named mystring2.
+11 should be assigned in the variable named myint.
+true should be assigned in the variable named myboolean.
+
+*/
+
+
+import java.lang.*;
+import java.util.*;
+
+public class Datatypes {
+    public static void main(String[] args) {
+        /*
+        Assign values to the following variable as described above
+        Don't change the variable name
+        */
+        String mystring1;
+        String mystring2;
+        int myint;
+        boolean myboolean;
+        
+        mystring1 = "Name";
+        mystring2 = "Don't change the variable name";
+        myint = 11;
+        myboolean = true;
+        /*Don't change the code below*/
+        System.out.println("String1 = " + mystring1);
+        System.out.println("String2 = " + mystring2);
+        System.out.println("int = " + myint);
+        System.out.println("boolean = " + myboolean);
+        
+    }
+}
