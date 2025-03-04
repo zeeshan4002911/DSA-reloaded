8541a88dbad7d5a089c51b84e9e48131a0018fe8
root <root>
2025-03-04 06:55:08 +0530

    added

---
+++ 1.data-structure/1.array/1.array-2d.py
@@ -0,0 +1,24 @@
+# Row level reverse of 2D-array (Matrix)
+
+
+def main():
+    A = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+    B = performOps(A)
+    for i in range(len(B)):
+        for j in range(len(B[i])):
+            print(B[i][j])
+
+def performOps(A):
+    m = len(A)
+    n = len(A[0])
+    B = []
+    for i in range(len(A)):
+        B.append([0] * n)
+        for j in range(len(A[i])):
+            B[i][n - 1 - j] = A[i][j]
+    return B
+
+
+
+if __name__ == "__main__":
+    main()
