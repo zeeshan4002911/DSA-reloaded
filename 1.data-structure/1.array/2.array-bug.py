3f6fcec76c11f39d4aba092837b7591490273b6e
zeesh <zeeshan4002911@gmail.com>
2025-03-04 07:24:22 +0530

    added

---
+++ 1.data-structure/1.array/2.array-bug.py
@@ -0,0 +1,17 @@
+class Solution:
+    # @param a : list of integers
+    # @param b : integer
+    # @return a list of integers
+    def rotateArray(self, a, b):
+        ret = []
+        for i in range(len(a)):
+            ret.append(a[(i + b) % len(a)])
+        return ret
+    
+if __name__ == "__main__":
+    sol = Solution()
+    A = [1,2,3,4,5,6]
+    B = 1
+    result = sol.rotateArray(A, B)
+    for val in result:
+        print(val, end=" ")
