1490d690187a7068af6f4563177abeaee1e92074
zeesh <zeeshan4002911@gmail.com>
2025-05-23 08:12:14 +0530

    easy - modified binary search

---
+++ 1.data-structure/1.array/easy/ceil-in-sorted-array.py
@@ -0,0 +1,60 @@
+"""
+Given a sorted array arr[] and an integer x, find the index (0-based) of the smallest element in arr[] that is greater than or equal to x. This element is called the ceil of x. If such an element does not exist, return -1.
+
+Note: In case of multiple occurrences of ceil of x, return the index of the first occurrence.
+
+Examples
+
+Input: arr[] = [1, 2, 8, 10, 11, 12, 19], x = 5
+Output: 2
+Explanation: Smallest number greater than 5 is 8, whose index is 2.
+
+Input: arr[] = [1, 2, 8, 10, 11, 12, 19], x = 20
+Output: -1
+Explanation: No element greater than 20 is found. So output is -1.
+
+Input: arr[] = [1, 1, 2, 8, 10, 11, 12, 19], x = 0
+Output: 0
+Explanation: Smallest number greater than 0 is 1, whose indices are 0 and 1. The index of the first occurrence is 0.
+
+Constraints:
+1 ≤ arr.size() ≤ 106
+1 ≤ arr[i] ≤ 106
+0 ≤ x ≤ arr[n-1]
+
+Expected Complexities
+Time Complexity: O(log n)
+Auxiliary Space: O(1)
+
+"""
+
+
+class Solution:
+    def modified_binary_search(self, arr, x):
+        size = len(arr)
+        result = -1
+        return self.helper(arr, 0, size - 1, x, result)
+
+    def helper(self, arr, low, high, target, result):
+        if low > high:
+            return result
+
+        mid = low + (high - low) // 2
+
+        if arr[mid] >= target:
+            # Possible answer, but check if there's a smaller index
+            return self.helper(arr, low, mid - 1, target, mid)
+        else:
+            return self.helper(arr, mid + 1, high, target, result)
+
+
+def main():
+    arr = [int(value) for value in input().strip().split()]
+    x = int(input())
+
+    solution = Solution()
+    print(solution.modified_binary_search(arr, x))
+
+
+if __name__ == "__main__":
+    main()
