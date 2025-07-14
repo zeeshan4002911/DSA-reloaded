4dfee4228fdec2ef349b65653133c38993cc301c
zeesh <zeeshan4002911@gmail.com>
2025-07-14 09:23:18 +0530

    easy - Added mathematical derivation for strictly increasing subarray formula calulcation

--- 1.data-structure/1.array/easy/count-strictly-increasing-subarray.py
+++ 1.data-structure/1.array/easy/count-strictly-increasing-subarray.py
@@ -27,19 +27,19 @@ class Solution:
         size = len(arr)
         result = 0
         window_size = 1
-        
+
         for i in range(1, size):
             # Expansion of window in case the sequence in stricly increasing
             if arr[i] > arr[i - 1]:
                 window_size += 1
             else:
                 # Possible subarray addition and resent of window length
-                result += ((window_size * (window_size - 1)) / 2)
+                result += (window_size * (window_size - 1)) / 2
                 window_size = 1
-        
+
         # Add the subarray for the end window in result count
-        result += ((window_size * (window_size - 1)) / 2)
-        
+        result += (window_size * (window_size - 1)) / 2
+
         return int(result)
 
 
@@ -51,3 +51,31 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+## Formula Derivation
+"""
+    *** Total Number of Subarrays ***
+    For a sequence of length n:
+    - Subarrays of length 1: There are n such subarrays (each individual element).
+    - Subarrays of length 2: There are n-1 such subarrays (each consecutive pair).
+    - Subarrays of length 3: There are n-2 such subarrays (each consecutive triplet), and so on, until:
+    - Subarrays of length n: There is exactly 1 subarray (the whole array itself).
+
+    For a strictly increasing sequence of length n, the total number of subarrays of all lengths (from 1 to n) is given by:
+    
+    Total subarrays = 1+2+3+⋯+n = n(n+1)/2
+
+    This counts all possible subarrays, including those of length 1.
+    
+    *** Subarrays of Length Greater Than 1 ***
+    To count only the subarrays of length greater than 1, we need to exclude the single-element subarrays (subarrays of length 1).
+        - The number of subarrays of length 1 is simply n (one for each element).
+        - The number of subarrays of length greater than 1 can be found by subtracting the single-element subarrays (length 1) from the total count.
+
+    So, the formula for counting subarrays of length greater than 1 is:
+    Subarrays of length > 1 = n(n+1)/2 - n
+    simplified version = n(n-1)/2
+    
+    same way total Subarray of length > 2 (more than 2 element in subarray) 
+    = n(n+1)/2 - n - (n - 1) = n(n-3)/2
+"""
