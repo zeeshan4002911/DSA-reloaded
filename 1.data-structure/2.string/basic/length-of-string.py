809740fca34e62f4efbf390898ab343c12a28ccd
zeesh <zeeshan4002911@gmail.com>
2025-07-17 09:10:04 +0530

    basic

---
+++ 1.data-structure/2.string/basic/length-of-string.py
@@ -0,0 +1,33 @@
+"""
+You are given a string s. You need to find the length of the string and return it.
+
+Examples:
+
+Input: s = Geeks
+Output: 5
+
+Input: s = Hello1234
+Output: 9
+
+Constraints:
+1 <= | s |  <= 100000
+
+Expected Complexities
+Time Complexity: O(1)
+Auxiliary Space: O(1)
+"""
+
+
+class Solution:
+    def length_of_string(self, s):
+        return len(s)
+
+
+def main():
+    s = input("Enter the String: ")
+    op = Solution()
+    print(op.length_of_string(s))
+
+
+if __name__ == "__main__":
+    main()
