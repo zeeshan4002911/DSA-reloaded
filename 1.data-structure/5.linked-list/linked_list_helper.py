91e545e7c5556493c1675d89334febe3df12896c
zeesh <zeeshan4002911@gmail.com>
2025-08-24 13:55:33 +0530

    Added append, has and insertAt methods

--- 1.data-structure/5.linked-list/linked_list_helper.py
+++ 1.data-structure/5.linked-list/linked_list_helper.py
@@ -1,5 +1,6 @@
 # Linked list implementation of python with basic operation using class and methods
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -21,6 +22,22 @@ class LinkedList:
 
         return self.head
 
+    # Add value into the end of linked list
+    def append(self, data, head=None):
+        curr = head or self.head
+        node = Node(data)
+
+        if curr is None:
+            head = node
+            self.head = node
+            return head or self.head
+
+        while curr.next is not None:
+            curr = curr.next
+        curr.next = node
+
+        return head or self.head
+
     # Print in pretty format
     def pretty_print(self, head=None):
         curr = head or self.head
@@ -40,6 +57,49 @@ class LinkedList:
             curr = curr.next
 
         return result
+
+    # Return the presencce boolean, works like in operator
+    def has_value(self, value, head=None):
+        curr = head or self.head
+
+        while curr is not None:
+            if curr.data == value:
+                return True
+            curr = curr.next
+
+        return False
+
+    # Insert value at any position of linked list,
+    # pos is 1 base and on greater than length of linked list it throws out of range error
+    def insert_at(self, head, pos, data):
+        node = Node(data)
+
+        # Insert at the beginning
+        if pos == 1:
+            node.next = head
+            head = node
+        else:
+            # Insert in-between of linked list
+            length = 0
+            curr, prev = head, None
+            while curr is not None:
+                length += 1
+                if length == pos:
+                    prev.next = node
+                    node.next = curr
+                    break
+
+                prev = curr
+                curr = curr.next
+
+            # Insert at the end of linked list
+            if pos == length + 1:
+                prev.next = node
+            # Raise index error exception on position value greater than size of linked list
+            else:
+                raise IndexError("Value of pos is out of range")
+            
+        return head
 
 
 class Node:
