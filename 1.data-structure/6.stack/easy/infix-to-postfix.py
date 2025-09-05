2fa433c6a5c97e6b6771198b299adacc1dba53fb
zeesh <zeeshan4002911@gmail.com>
2025-09-05 23:54:21 +0530

    medium

---
+++ 1.data-structure/6.stack/easy/infix-to-postfix.py
@@ -0,0 +1,81 @@
+"""
+Given an infix expression in the form of string s. Convert this infix expression to a postfix expression.
+
+    Infix expression: The expression of the form a op b. When an operator is in between every pair of operands.
+    Postfix expression: The expression of the form a b op. When an operator is followed for every pair of operands.
+
+Note: The order of precedence is: ^ greater than * equals to / greater than + equals to -. Ignore the right associativity of ^.
+
+Examples :
+
+Input: s = "a+b*(c^d-e)^(f+g*h)-i"
+Output: abcd^e-fgh*+^*+i-
+Explanation: After converting the infix expression into postfix expression, the resultant expression will be abcd^e-fgh*+^*+i-
+
+Input: s = "A*(B+C)/D"
+Output: ABC+*D/
+Explanation: After converting the infix expression into postfix expression, the resultant expression will be ABC+*D/
+
+Input: s = "(a+b)*(c+d)"
+Output: ab+cd+*
+
+Constraints:
+1 ≤ s.length ≤ 30
+
+Expected Complexities
+Time Complexity: O(n)
+Auxiliary Space: O(n)
+"""
+
+from collections import deque
+
+
+class Solution:
+    def infix_to_postfix_expression(self, s):
+        result = ""
+        size = len(s)
+        st = deque()
+
+        for i in range(size):
+            # Adding all the variables or number into the result
+            if str(s[i]).isalnum():
+                result += s[i]
+            
+            # Processing for bracket element
+            elif s[i] == "(":
+                st.append(s[i])
+            elif s[i] == ")":
+                while st and st[-1] != "(":
+                    result += st.pop()
+                st.pop()
+            
+            # For operator
+            else:
+                while st and self.precedence_check(s[i]) <= self.precedence_check(st[-1]):
+                    result += st.pop()
+                st.append(s[i])
+
+        # Adding remaining element of stack
+        while st:
+            result += st.pop()
+
+        return result
+
+    def precedence_check(self, char):
+        if char == "^":
+            return 3
+        elif char in ["*", "/"]:
+            return 2
+        elif char in ["+", "-"]:
+            return 1
+        return -1
+
+
+def main():
+    s = input("Enter infix expression: ").strip()
+    solution = Solution()
+    print(solution.infix_to_postfix_expression(s))
+
+
+if __name__ == "__main__":
+    main()
