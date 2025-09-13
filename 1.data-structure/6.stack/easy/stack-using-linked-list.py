7e00ee082e3eae9a6f3e37c1e3a866c021328c61
zeesh <zeeshan4002911@gmail.com>
2025-09-13 07:46:16 +0530

    Changed top of stack to head of linked list

--- 1.data-structure/6.stack/easy/stack-using-linked-list.py
+++ 1.data-structure/6.stack/easy/stack-using-linked-list.py
@@ -1,5 +1,5 @@
 """
-Implement a stack using doubly linked list. The stack should support the following operations:
+Implement a stack using linked list. The stack should support the following operations:
 
     push(x): Insert element x onto the top of the stack.
     pop(): Remove the element from the top of the stack. If the stack is empty, do nothing.
@@ -8,22 +8,17 @@ Implement a stack using doubly linked list. The stack should support the followi
 """
 
 
-class CustomStack:
+class Stack:
     def __init__(self):
         self._top = None
         self._size = 0
 
     def push(self, val):
         node = Node(val)
-        if self._top is None:
-            self._top = node
-        else:
-            # Connect node to end of linked list
-            self._top.next = node
-            # Connect the new node prev to current head
-            node.prev = self._top
-            # Moving the head to next node
-            self._top = self._top.next
+        # Connect node to beginning of linked list
+        node.next = self._top
+        # Assigning the head to new node
+        self._top = node
 
         self._size += 1
         return f"{val} pushed successfully"
@@ -34,28 +29,33 @@ class CustomStack:
 
         # Cache value before removing the node
         data = self._top.data
-        # Traversing back in linked list using prev pointer
-        self._top = self._top.prev
-        # Detaching the next linked to break the link of last node
+        # Cache next node for traversing forward in linked list
+        next_node = self._top.next
+        # Detaching the next linked to break the link of head node
         self._top.next = None
+        # Assigning the head to next node
+        self._top = next_node
 
         self._size -= 1
         return f"{data} popped successfully"
 
     def top(self):
         if self._top is None:
-            return "Underflow Error"
+            return "Underflow Error: Stack is empty"
         return f"Stack peek : {self._top.data}"
 
     def size(self):
         return self._size
+    
+    def is_empty(self):
+        return self._top is None
 
     def print_stack(self):
         result = []
         curr = self._top
         while curr is not None:
             result.append(curr.data)
-            curr = curr.prev
+            curr = curr.next
         return result
 
 
@@ -63,26 +63,27 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
-        self.prev = None
 
 
 def main():
-    c_st = CustomStack()
+    st = Stack()
     while True:
-        print("push(a) / pop(r) / top(t) / size(s) / display(d) / exit(e)")
+        print("push(a) / pop(r) / top(t) / size(s) / isEmpty(e) / display(d) / exit(q)")
 
         user_input = input().strip().split()[:2]
         if user_input[0] == "a":
-            print(c_st.push(user_input[1]))
+            print(st.push(user_input[1]))
         elif user_input[0] == "r":
-            print(c_st.pop())
+            print(st.pop())
         elif user_input[0] == "t":
-            print(c_st.top())
+            print(st.top())
         elif user_input[0] == "s":
-            print(c_st.size())
-        elif user_input[0] == "d":
-            print(c_st.print_stack())
+            print(st.size())
         elif user_input[0] == "e":
+            print(st.is_empty())
+        elif user_input[0] == "d":
+            print(st.print_stack())
+        elif user_input[0] == "q":
             break
 
 
