8534aefcdb1027816a1800d8dc64f72f4bb83657
zeesh <zeeshan4002911@gmail.com>
2025-11-14 06:49:19 +0530

    easy

---
+++ 1.data-structure/9.tree/1.binary-tree/basic/level-order-traversal.py
@@ -0,0 +1,65 @@
+"""
+Given the root of a Binary Tree, your task is to return its Level Order Traversal.
+
+Note: A level order traversal is a breadth-first search (BFS) of the tree. It visits nodes level by level, starting from the root, and processes all nodes from left to right within each level before moving to the next.
+
+Examples:
+
+Input: root = [1, 2, 3]
+
+Output: [[1], [2, 3]]
+Explanation: We start with the root node 1, so the first level of the traversal is [1]. Then we move to its children 2 and 3, which form the next level, giving the final output [[1], [2, 3]].
+
+Input: root = [10, 20, 30, 40, 50]
+
+Output: [[10], [20, 30], [40, 50]]
+Explanation: We begin with the root node 10, which forms the first level as [10]. Its children 20 and 30 make up the second level, and their children 40 and 50 form the third level, resulting in [[10], [20, 30], [40, 50]].
+
+Constraints:
+1 ≤ number of nodes ≤ 3*10^4
+0 ≤ node->data ≤ 10^9
+
+Expected Complexities
+Time Complexity: O(n)
+Auxiliary Space: O(n)
+"""
+
+import sys, os
+
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."))
+from binary_tree_helper import BinaryTree
+
+
+class Traversal:
+    def level_order_traversal(self, root):
+        return self._helper(root, 0, [])
+
+    def _helper(self, node, curr_level, levels):
+        if node is None:
+            return levels
+
+        if len(levels) <= curr_level:
+            levels.append([])
+        
+        levels[curr_level].append(node.data)
+
+        self._helper(node.left, curr_level + 1, levels)
+        self._helper(node.right, curr_level + 1, levels)
+
+        return levels
+
+
+def main():
+    arr = input("Enter tree in form of level order list: ").strip().split()
+    arr = [None if val == "N" else val for val in arr]
+    bt = BinaryTree()
+    root = bt.build_tree(arr)
+    # bt.print_tree()
+
+    traversal = Traversal()
+    result = traversal.level_order_traversal(root)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
