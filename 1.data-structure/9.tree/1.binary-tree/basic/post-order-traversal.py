0bc4618b5c5d2c7c9c3588b1f5abb44cb90fde7f
zeesh <zeeshan4002911@gmail.com>
2025-12-12 07:23:12 +0530

    updated - added stack based solution

--- 1.data-structure/9.tree/1.binary-tree/basic/post-order-traversal.py
+++ 1.data-structure/9.tree/1.binary-tree/basic/post-order-traversal.py
@@ -6,11 +6,22 @@ Note: A postorder traversal first visits the left child (including its entire su
 Examples:
 
 Input: root = [19, 10, 8, 11, 13]
+     19
+    /  \
+   10   8
+  / \
+11   13
 
 Output: [11, 13, 10, 8, 19]
 Explanation: The postorder traversal of the given binary tree is [11, 13, 10, 8, 19].
 
 Input: root = [11, 15, N, 7]
+
+     11
+    /  \
+   15   N
+  /
+7
 
 Output: [7, 15, 11]
 Explanation: The postorder traversal of the given binary tree is [7, 15, 11].
@@ -25,13 +36,14 @@ Auxiliary Space: O(1)
 """
 
 import sys, os
+from queue import LifoQueue
 
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."))
 from binary_tree_helper import BinaryTree
 
 
 class Traversal:
-    def post_order_traversal(self, root):
+    def post_order_traversal_rec(self, root):
         return self._helper(root, [])
 
     def _helper(self, root, res):
@@ -44,6 +56,27 @@ class Traversal:
 
         return res
 
+    def post_order_traversal_ittr(self, root):
+        if root is None:
+            return []
+
+        result = []
+        stack = LifoQueue()
+        stack.put(root)
+
+        # Modified pre-order traversal [root->right->left] + reverse
+        while not stack.empty():
+            curr = stack.get()
+            result.append(curr.data)
+
+            if curr.left is not None:
+                stack.put(curr.left)
+
+            if curr.right is not None:
+                stack.put(curr.right)
+
+        return result[::-1]
+
 
 def main():
     arr = input("Enter tree in form of level order list: ").strip().split()
@@ -53,7 +86,8 @@ def main():
     # bt.print_tree()
 
     traversal = Traversal()
-    result = traversal.post_order_traversal(root)
+    # result = traversal.post_order_traversal_rec(root)
+    result = traversal.post_order_traversal_ittr(root)
     print(result)
 
 
