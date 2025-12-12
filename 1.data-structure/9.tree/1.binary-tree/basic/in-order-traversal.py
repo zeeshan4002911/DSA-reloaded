abcff81b8b95bbe101b76e067c325ca950aed324
zeesh <zeeshan4002911@gmail.com>
2025-12-12 06:55:42 +0530

    updated - exmaple

--- 1.data-structure/9.tree/1.binary-tree/basic/in-order-traversal.py
+++ 1.data-structure/9.tree/1.binary-tree/basic/in-order-traversal.py
@@ -6,11 +6,26 @@ Note: An inorder traversal first visits the left child (including its entire sub
 Examples:
 
 Input: root = [1, 2, 3, 4, 5]
+    1
+   /  \
+  2    3
+ /  \
+4    5
 
 Output: [4, 2, 5, 1, 3]
 Explanation: The inorder traversal of the given binary tree is [4, 2, 5, 1, 3].
 
 Input: root = [8, 1, 5, N, 7, 10, 6, N, 10, 6]
+
+        8
+       / \
+      /   \
+     /     \
+    1        5
+   / \      /  \
+ N   7     10  6
+    / \     /
+   N   10  6 
 
 Output: [1, 7, 10, 8, 6, 10, 5, 6]
 Explanation: The inorder traversal of the given binary tree is [1, 7, 10, 8, 6, 10, 5, 6].
