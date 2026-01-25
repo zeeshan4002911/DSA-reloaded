4614b52d0f3966b00dc2fed3681696a4867b7a8a
zeesh <zeeshan4002911@gmail.com>
2026-01-25 20:05:54 +0530

    added

---
+++ 1.data-structure/9.tree/2.binary-search-tree.py/basic/floor-in-bst.py
@@ -0,0 +1,89 @@
+"""
+You are given a BST(Binary Search Tree) with n number of nodes and value x. your task is to find the greatest value node of the BST which is smaller than or equal to x.
+Note: when x is smaller than the smallest node of BST then returns -1.
+
+Examples:
+
+Input:
+n = 7               2
+                     \
+                      81
+                    /     \
+                 42       87
+                   \       \
+                    66      90
+                   /
+                 45
+x = 87
+Output: 87
+Explanation: 87 is present in tree so floor will be 87.
+
+Input:
+n = 4                     6
+                           \
+                            8
+                          /   \
+                        7       9
+x = 11
+Output: 9
+
+Input:
+n = 4                     6
+                           \
+                            8
+                          /   \
+                        7       9
+x = 5
+Output: -1
+
+Constraint:
+1 <= Node data <= 10^9
+1 <= n <= 10^5
+
+Expected Complexities
+Time Complexity: O(height of tree)
+Auxiliary Space: O(height of tree)
+"""
+
+import os, sys
+
+sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), ".."))
+from binary_search_tree_helper import BinarySearchTree
+
+
+class Solution:
+    def get_floor(self, root, x):
+        if root is None:
+            return -1
+        
+        curr = root
+        floor_value = -1
+        while curr:
+            # Capturing the floor value of the current node data before going to next
+            if curr.data <= x:
+                floor_value = curr.data
+            
+            if curr.data < x:
+                curr = curr.right
+            elif curr.data > x:
+                curr = curr.left
+            else:
+                return curr.data
+
+        return floor_value
+
+def main():
+    arr = input("Enter the tree level order: ").strip().split()
+    x = int(input("Enter the value of x: ").strip())
+    arr = [x.replace(",", "").strip() for x in arr]
+    bst = BinarySearchTree()
+    root = bst.build_bst(arr)
+    bst.print_tree()
+
+    soln = Solution()
+    result = soln.get_floor(root, x)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
