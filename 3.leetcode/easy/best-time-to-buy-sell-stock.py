8986ec857cad9a077f777bb6d7e894402b51fb49
zeesh <zeeshan4002911@gmail.com>
2026-06-10 09:33:21 +0530

    added - array and basic greedy

---
+++ 3.leetcode/easy/best-time-to-buy-sell-stock.py
@@ -0,0 +1,66 @@
+"""
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+
+
+
+Constraints:
+
+    1 <= prices.length <= 10^5
+    0 <= prices[i] <= 10^4
+
+"""
+
+from typing import List
+
+
+class Solution:
+    def max_profit(self, prices: List[int]) -> int:
+        if len(prices) == 0:
+            return 0
+        
+        profit = 0
+        buy_price = prices[0]
+
+        for curr_price in prices:
+            # Sell stock if profit is more than previous profit
+            if curr_price - buy_price > profit:
+                profit = curr_price - buy_price
+
+            # Buy stock if stock price is less than earlier buy
+            if curr_price < buy_price:
+                buy_price = curr_price
+
+        return profit
+
+
+def main():
+    prices = input("Enter prices: ").strip()
+
+    prices = prices.replace(",", " ").split()
+    prices = list(map(int, prices))
+
+    soln = Solution()
+    print(soln.max_profit(prices))
+
+
+if __name__ == "__main__":
+    main()
